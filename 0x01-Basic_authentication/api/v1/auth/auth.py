d40702791a808c5dd480e3080fd5097e44f408b6
GODSPE1 <godspy18@gmail.com>
2024-08-06 12:18:58 +0100

    pycodestyle standard

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,14 +8,15 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth class for managing API authentication"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required for a given path.
 
         Args:
             path (str): The path to check.
-            excluded_paths (List[str]): A list of paths that do not require authentication.
+            excluded_paths (List[str]): list of paths that
+            do not require authentication.
 
         Returns:
             bool: True if authentication is required, False otherwise.
@@ -24,16 +25,16 @@ class Auth:
             return True
         if excluded_paths is None or not excluded_paths:
             return True
-        
+
         # Ensure path ends with a slash for comparison
         if not path.endswith('/'):
             path += '/'
-        
+
         # Check if the path is in excluded_paths
         for excluded_path in excluded_paths:
             if excluded_path.endswith('/') and path == excluded_path:
                 return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
