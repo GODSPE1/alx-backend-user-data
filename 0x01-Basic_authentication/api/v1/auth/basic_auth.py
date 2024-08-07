7d53e932b9922b833c952a6727075362a30298d6
GODSPE1 <godspy18@gmail.com>
2024-08-07 08:47:46 +0100

    add documentation to extract_base64_authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,8 +9,12 @@ from models.user import User
 class BasicAuth(Auth):
     """returns the Base64 part of the Authorization
     header for a Basic Authentication"""
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """"returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+
         if authorization_header is None:
             return None
         if type(authorization_header) != str:
