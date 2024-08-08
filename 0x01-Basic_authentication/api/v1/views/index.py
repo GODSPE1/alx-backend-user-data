19786f76e91b7c2c490e215077038aa8a09b9e16
GODSPE1 <godspy18@gmail.com>
2024-08-08 07:25:56 +0100

    add the type of method

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,7 +14,7 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats/', methods=['GET'], strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('api/v1/unauthorized', methods=['GET'])
+@app_views.route('api/v1/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """GET /api/v1/unauthorized
     Raises:
@@ -35,7 +35,7 @@ def unauthorized():
     abort(401)
 
 
-@app_views.route('/api/v1/forbidden', methods=['GET'])
+@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden():
     """GET /api/v1/forbidden
     Raises:
