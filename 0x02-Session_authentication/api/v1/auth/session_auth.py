656e7851007273ef2ab5120a11e2f9747aed7186
GODSPE1 <godspy18@gmail.com>
2024-08-11 02:27:21 +0100

    made pycodestyle standard

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """Authenticate a user using the sassion authentication"""
 
@@ -15,7 +16,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """This Function returs a user ID Based on the session ID
         Parameter:
@@ -25,7 +26,6 @@ class SessionAuth(Auth):
         """
         if not session_id or not isinstance(session_id, str):
             return None
-        
-        
+
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
