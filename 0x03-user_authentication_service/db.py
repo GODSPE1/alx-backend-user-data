9fd360217b135bab3303706b148e580a8810e0ad
GODSPE1 <godspy18@gmail.com>
2024-08-15 11:54:16 +0100

    add a method to locate a user by id, update and commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError, SQLAlchemyError
 from user import Base, User
+from typing import Dict
 
 
 class DB:
@@ -29,8 +32,6 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-
-    @property
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Parameter:
@@ -42,3 +43,38 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+    
+    def find_user_by(self, **kwargs: Dict) -> User:
+        """Find a user by email or hashed password"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise e
+    
+    def update_user(self, user_id: int, **kwargs: Dict) -> None:
+        """Locate the user by id, update and commit to database
+        Parameter:
+            user_id - id of user to update
+            **kwargs - key, value pairs of attribute to update
+
+        Raises: raises value error if invalid attribute is passed
+        """
+        try:
+            user = find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError(f"User with id {user_id} not found")
+        
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Invalid attribute: {key}")
+            setattr(user, key, value)
+        try:
+            self._session.commit()
+        except SQLAlchemyError as e:
+            self._session.rollback()
+            raise e
